6db5275504e232858da7dad54878b2f430f3aba8
Khieu Thi Chien <huuxinhtrai18@gmail.com>
2018-12-08 00:24:39 +0700

    anonymous_7/12/2018

--- app/src/main/java/com/khieuthichien/thibanglaixemay/ui/MainActivity.java
+++ app/src/main/java/com/khieuthichien/thibanglaixemay/ui/MainActivity.java
@@ -1,16 +1,24 @@
 package com.khieuthichien.thibanglaixemay.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.khieuthichien.thibanglaixemay.R;
 import com.khieuthichien.thibanglaixemay.database.DatabaseHelper;
 
 import java.io.IOException;
+import java.security.MessageDigest;
 import java.util.Timer;
 import java.util.TimerTask;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +49,25 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
+        printHashKey(MainActivity.this);
 
     }
+
+    public static void printHashKey(Context context) {
+        try {
+            final PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                final MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                final String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i("AppLog", "key:" + hashKey + "=");
+            }
+        } catch (Exception e) {
+            Log.e("AppLog", "error:", e);
+        }
+    }
+
+
+
+
 }
--- app/src/main/java/com/khieuthichien/thibanglaixemay/ui/MenuActivity.java
+++ app/src/main/java/com/khieuthichien/thibanglaixemay/ui/MenuActivity.java
@@ -1,18 +1,31 @@
 package com.khieuthichien.thibanglaixemay.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.ImageView;
 
-import com.facebook.share.model.ShareHashtag;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.facebook.share.model.ShareLinkContent;
 import com.khieuthichien.thibanglaixemay.R;
 
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -22,11 +35,66 @@ public class MenuActivity extends AppCompatActivity {
     private ImageView imgmeoghinho;
     private ImageView imgchiaseungdung;
     private ImageView imggioithieu;
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        //logger.logPurchase(BigDecimal.valueOf(4.32), Currency.getInstance("USD"));
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        // If using in a fragment
+        //loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
 
         imgthisathach = findViewById(R.id.imgthisathach);
         imghoclythuyet = findViewById(R.id.imghoclythuyet);
@@ -74,25 +142,12 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                ShareLinkContent content = new ShareLinkContent.Builder()
-                        .setContentUrl(Uri.parse("https://developers.facebook.com"))
-                        .build();
-
-                ShareLinkContent.Builder content1 = new ShareLinkContent.Builder()
-                        .setContentUrl(Uri.parse("https://developers.facebook.com"))
-                        .setShareHashtag(new ShareHashtag.Builder()
-                                .setHashtag("#ConnectTheWorld")
-                                .build());
-
-
-
                 Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                Uri screenshotUri = Uri.parse("android.resource://comexample.sairamkrishna.myapplication/*");
+                Uri screenshotUri = Uri.parse("android.resource://comexample.sairamkrishna.myapplication/https://play.google.com/store/apps/details?id=com.vietdevpro.onthigiaypheplaixe.xemay");
 
                 try {
                     InputStream stream = getContentResolver().openInputStream(screenshotUri);
                 }
-
                 catch (FileNotFoundException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -101,8 +156,17 @@ public class MenuActivity extends AppCompatActivity {
                 sharingIntent.setType("image/jpeg");
                 sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
                 startActivity(Intent.createChooser(sharingIntent, "Chia sẻ ứng dụng"));
+
             }
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
